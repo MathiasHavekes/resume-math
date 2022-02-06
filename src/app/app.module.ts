d1a7f60cecdcaa1a04fa4c630d1cf4cd20d920d0
Mathias Havekes <mathiashavekes@gmail.com>
2022-02-06 14:29:55 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ExperienceComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule
